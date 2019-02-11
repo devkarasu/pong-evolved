49751ce65bf38304f2051375b4f44a5b364ebc41
Mashiro <devkarasu@gmail.com>
2019-02-11 21:57:10 +0900

    スタート画面の追加

--- ponpon.cpp
+++ ponpon.cpp
@@ -5,11 +5,17 @@
 
 void display();
 void reshape(int w, int h);
+void keyboard(unsigned char key, int x, int y);
 void specialkeyDown(int key, int x, int y);
 void specialkeyUp(int key, int x, int y);
 void timer(int t);
 void timer_gameover1(int t);
 void timer_gameover2(int t);
+void printScore();
+void printRule();
+
+enum GameState { START, PLAY, GAMEOVER };
+GameState gamestate = START;
 
 Ball ball;
 Particles p;
@@ -20,7 +26,6 @@ bool isPressR = false;
 bool isPressU = false;
 bool isPressD = false;
 
-bool isGameover = false;
 int score = 0;
 
 // Initialization
@@ -34,6 +39,7 @@ void GLUT_CALL_FUNC() {
   glutDisplayFunc(display);
   glutReshapeFunc(reshape);
   glutTimerFunc(0, timer, 20);
+  glutKeyboardFunc(keyboard);
   glutSpecialFunc(specialkeyDown);
   glutSpecialUpFunc(specialkeyUp);
 }
@@ -49,6 +55,8 @@ int main(int argc, char *argv[]) {
   GLUT_INIT();
   GLUT_CALL_FUNC();
   MY_INIT();
+
+  printRule();
 
   glutMainLoop();
 
@@ -66,34 +74,27 @@ void display() {
   // 軸の表示
   //drawAxis();
 
-  // 物体の色
-  if (isGameover)
-    glColor3d(1, 0, 0);
-  else
+  switch (gamestate) {
+  case START:
     glColor3d(0.0, 0.7, 0.0);
-
-  // 枠の表示
-  bar_x.draw();
-  bar_z.draw();
-
-  // 玉の表示
-  if (!isGameover)
+    bar_x.draw();
+    bar_z.draw();
+    break;
+  case PLAY:
+    glColor3d(0.0, 0.7, 0.0);
+    bar_x.draw();
+    bar_z.draw();
     ball.draw();
-
-  // パーティクルの表示
-  p.draw();
-
-  // スコア表示
-  void *font = GLUT_BITMAP_HELVETICA_18;
-  std::string cscore = std::to_string(score);
-  if (cscore.length() == 1) {
-    glRasterPos3d(0, 0, 0);
-    glutBitmapCharacter(font, cscore[0]);
-  }
-  else {
-    glRasterPos3d(-0.5, 0, 0);
-    for (char x : cscore)
-      glutBitmapCharacter(font, x);
+    p.draw();
+    printScore();
+    break;
+  case GAMEOVER:
+    glColor3d(1, 0, 0);
+    bar_x.draw();
+    bar_z.draw();
+    p.draw();
+    printScore();
+    break;
   }
 
   glFlush();
@@ -108,6 +109,20 @@ void reshape(int w, int h) {
   glLoadIdentity();
   gluPerspective(30.0, (double)w / (double)h, 1.0, 100.0);
   glMatrixMode(GL_MODELVIEW);
+}
+
+void keyboard(unsigned char key, int x, int y) {
+  if (gamestate == START || gamestate == GAMEOVER) {
+    if (key == 's' || key == 'S') {
+      std::cout << "Start\n";
+      ball = Ball();
+      p.data.erase(std::begin(p.data), std::end(p.data));
+      score = 0;
+      if (gamestate == GAMEOVER)
+        glutTimerFunc(20, timer, 20);
+      gamestate = PLAY;
+    }
+  }
 }
 
 void specialkeyDown(int key, int x, int y) {
@@ -145,39 +160,45 @@ void specialkeyUp(int key, int x, int y) {
 }
 
 void timer(int t) {
-  if (isPressU && isPressD);
-  else if (isPressU)
-    bar_x.moveLeft(0.02);
-  else if (isPressD)
-    bar_x.moveRight(0.02);
-
-  if (isPressL && isPressR);
-  else if (isPressL)
-    bar_z.moveRight(0.02);
-  else if (isPressR)
-    bar_z.moveLeft(0.02);
-  
-  int zc = bar_z.checkHit(ball);
-  int xc = bar_x.checkHit(ball);
-  if (zc == 1) {
-    ++score;
-    p.addParticles(100, ball.x, 0, ball.z);
-  }
-  if (xc == 1) {
-    ++score;
-    p.addParticles(100, ball.x, 0, ball.z);
-  }
-
-  if (zc == 0 || xc == 0)
-    isGameover = true;
-
-  glutPostRedisplay();
-  if (!isGameover)
-    glutTimerFunc(t, timer, 20);
-  else {
-    p.addParticles(5000, ball.x, 0, ball.z);
+  if (gamestate == START) {
     glutPostRedisplay();
-    glutTimerFunc(1000, timer_gameover1, 20);
+    glutTimerFunc(t, timer, 20);
+  }
+  else if (gamestate == PLAY) {
+    if (isPressU && isPressD);
+    else if (isPressU)
+      bar_x.moveLeft(0.02);
+    else if (isPressD)
+      bar_x.moveRight(0.02);
+
+    if (isPressL && isPressR);
+    else if (isPressL)
+      bar_z.moveRight(0.02);
+    else if (isPressR)
+      bar_z.moveLeft(0.02);
+    
+    int zc = bar_z.checkHit(ball);
+    int xc = bar_x.checkHit(ball);
+    if (zc == 1) {
+      ++score;
+      p.addParticles(100, ball.x, 0, ball.z);
+    }
+    if (xc == 1) {
+      ++score;
+      p.addParticles(100, ball.x, 0, ball.z);
+    }
+
+
+    if (zc == 0 || xc == 0) {
+      gamestate = GAMEOVER;
+      p.addParticles(5000, ball.x, 0, ball.z);
+      glutPostRedisplay();
+      glutTimerFunc(1000, timer_gameover1, 20);
+    }
+    else {
+      glutPostRedisplay();
+      glutTimerFunc(t, timer, 20);
+    }
   }
 }
 
@@ -187,5 +208,28 @@ void timer_gameover1(int t) {
 
 void timer_gameover2(int t) {
   glutPostRedisplay();
-  glutTimerFunc(20, timer_gameover2, 20);
+  if(gamestate == GAMEOVER)
+    glutTimerFunc(20, timer_gameover2, 20);
+}
+
+void printScore() {
+  // スコア表示
+  void *font = GLUT_BITMAP_HELVETICA_18;
+  std::string cscore = std::to_string(score);
+  if (cscore.length() == 1) {
+    glRasterPos3d(0, 0, 0);
+    glutBitmapCharacter(font, cscore[0]);
+  }
+  else {
+    glRasterPos3d(-0.5, 0, 0);
+    for (char x : cscore)
+      glutBitmapCharacter(font, x);
+  }
+}
+
+void printRule() {
+  std::cout << "--------------Control-----------------" << std::endl;
+  std::cout << "'s' key : GAME START" << std::endl;
+  std::cout << "Arrow key : Move Bar" << std::endl;
+  std::cout << "--------------------------------------" << std::endl;
 }
